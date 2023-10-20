ef97f5bf9497b30fe44a7015b835cdbf0465cb5a
MalcarAle <alexandre.malcar@gmail.com>
2023-10-20 16:25:48 -0300

    Adicionado rota de teste e conexão com banco de dados

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 require("dotenv").config()
+const knex = require("./database/connection")
 const express = require("express")
 
 const app = express()
 
 app.use(express.json())
 
-app.get("/", async (req, res) => {
-  return res.json("API está ok! ")
+app.get("/carros", async (req, res) => {
+  const carros = await knex("carros")
+
+  return res.json(carros)
 })
 
 app.listen(process.env.PORT, () => {
